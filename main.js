50d03a245c22543f9f65e51e08ad9b6ee70c4ce9
Heather Vandervecht <heathervandervecht@gmail.com>
2016-07-25 22:15:32 -0400

    feat: hide and show emojis in ui based on returned js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // DOM utility functions
 const addClass = (el, newClass) => {
-  el.className += ` ${newClass}`;
+  el.classList.add(newClass);
 };
 
 const removeClass = (el, oldClass) => {
@@ -16,6 +16,7 @@ const removeClass = (el, oldClass) => {
   const inputField = document.getElementsByTagName('input')[0];
   let typeTimeout;
   const TYPE_INTERVAL = 10000;
+  const siteContainer = document.getElementsByTagName('body');
 
   inputField.addEventListener("input", (event) => {
     const value = event.target.value;
@@ -23,19 +24,46 @@ const removeClass = (el, oldClass) => {
 
     if (value) {
       typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
+    } else {
+      removeClass(siteContainer[0], 'filtering');
+      
+      const prevFilter = document.getElementsByClassName('visible');
+    
+      oldEmojis(prevFilter);
     }
   }, false);
 
   const matchKeyword = (value) => {
     let matchedEmojiTitles = [];
     const matchedObjects = EMOJIS.filter(filterByKeyword.bind(this, value));
+
+
     if (matchedObjects.length == 0) return;
 
     matchedObjects.map((obj) => {
       matchedEmojiTitles.push(obj.keywords[0]);
     });
 
-    // do something with matchedEmojiTitles here
+    const prevFilter = document.getElementsByClassName('visible');
+
+    oldEmojis(prevFilter);
+
+
+    for (const emoji of matchedEmojiTitles) {
+      const emojiItem = document.querySelector('[title="' + emoji + '"]');
+
+      if (emojiItem) {
+        addClass(emojiItem.parentElement,'visible');
+      }
+    }
+
+    addClass(siteContainer[0], 'filtering');
+  }
+
+  const oldEmojis = (value) => {
+    for (const prev of value) {
+      removeClass(prev, 'visible');
+    };
   }
 
   const filterByKeyword = (value, obj) => {
