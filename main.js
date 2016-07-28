954b0736283237a9adc13ae4813c73d4e0b99d64
Heather Vandervecht <heathervandervecht@gmail.com>
2016-07-27 22:56:09 -0400

    doc: comments everywhere

--- main.js
+++ main.js
@@ -34,40 +34,53 @@ const removeAllElements = elements => {
 
 // Search Script
 (() => {
+  // Set all of our base variables (to be used throughout the function)
   const inputField = document.getElementsByTagName('input')[0];
   let typeTimeout;
   const TYPE_INTERVAL = 10000;
-  // Set variable for filtering purposes
   const siteContainer = document.getElementsByTagName('body')[0];
   const messageToClick = document.getElementsByClassName('message--click')[0];
   const messageConfirm = document.getElementsByClassName('message--confirm')[0];
 
+  // Event listener on input field
   inputField.addEventListener("input", (event) => {
+    // Set input value to a variable
     const value = event.target.value;
-    // Set variable of any emojis set to "visible" (this returns elements if a previous search has already been done)
+    // Set variable of any existing emojis set to "visible" (this returns elements if a previous search has already been done)
     const prevFilter = document.getElementsByClassName('visible');
+
+    // Reset timeout when user types in input
     if (typeTimeout) window.clearTimeout(typeTimeout);
 
     // Reset the previous search results
     resetOldEmojis(prevFilter);
+
+    // Hide confirm message (copy success message)
     addClass(messageConfirm, 'hidden');
 
+    // If there is a value, set a timeout before matching keyword
     if (value) {
       typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
     } else {
-      // Because the input has no value (and therefore we're not filtering), let's remove the filtering class from our site container
+      // If there isn't a value, remove the filtering class from siteContainer and hide the "copy to clipboard" message
       removeClass(siteContainer, 'filtering');
       addClass(messageToClick, 'hidden');
 
+      // Get all inputs and if there are any, remove them (these are for copying)
       const hiddenFields = document.getElementsByClassName('copy--input');
       if (hiddenFields.length > 0) removeAllElements(hiddenFields);
     }
   }, false);
 
   const matchKeyword = (value) => {
+    // Create emoji array
     let matchedEmojiTitles = [];
+
+    // Look through emoji array (emoji.js) and filter by input value
+      // Extra breakdown: filter() creates another array of items that return TRUTHY from filterByKeyword() - which we're setting to matchedObjects
     const matchedObjects = EMOJIS.filter(filterByKeyword.bind(this, value));
 
+    // If no emoji matches, end the function
     if (matchedObjects.length == 0) return;
 
     // Looping through the array of emojis that match the search term
@@ -81,6 +94,8 @@ const removeAllElements = elements => {
   }
 
   const filterByKeyword = (value, obj) => {
+    // Look at keywords in emoji.js and if they match value (from input) -> return true
+      // Extra breakdown: some() loops through an array and only returns a truthy value when the conditions are met (ie when value === keyword)
     return obj.keywords.some((keyword) => {
       return value === keyword;
     });
