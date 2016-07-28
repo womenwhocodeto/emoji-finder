bbea82a482b44b861d8912740e9d1daef1f7f77b
Heather Vandervecht <heathervandervecht@gmail.com>
2016-07-27 22:07:34 -0400

    EXERCISE: input event listener

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// DOM utility functions
-const addClass = (el, newClass) => {
-  el.className += ` ${newClass}`;
-};
-
-const removeClass = (el, oldClass) => {
-  const currentClasses = el.className.split(" ");
-
-  el.className = currentClasses.filter(names =>
-    names.trim() !== "" && names !== oldClass
-  ).join(" ");
-};
-
 // Search Script
 (() => {
   const inputField = document.getElementsByTagName('input')[0];
@@ -18,33 +5,28 @@ const removeClass = (el, oldClass) => {
   const TYPE_INTERVAL = 10000;
 
   inputField.addEventListener("input", (event) => {
-    const value = event.target.value;
-    if (typeTimeout) window.clearTimeout(typeTimeout);
+    // 1. Get value of input
 
-    if (value) {
-      typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
-    }
+    // 2. Reset timeout
+
+    // 3. Send value onward and set a timeout
   }, false);
-
-  const matchKeyword = (value) => {
-    let matchedEmojiTitles = [];
-    const matchedObjects = EMOJIS.filter(filterByKeyword.bind(this, value));
-    if (matchedObjects.length == 0) return;
-
-    matchedObjects.map((obj) => {
-      matchedEmojiTitles.push(obj.keywords[0]);
-    });
-
-    // do something with matchedEmojiTitles here
-  }
-
-  const filterByKeyword = (value, obj) => {
-    return obj.keywords.some((keyword) => {
-      return value === keyword;
-    });
-  }
 
   const delay = (func, delay) => {
     window.setTimeout(func, delay);
   }
 })();
+
+
+
+
+
+
+
+
+// const value = event.target.value;
+// if (typeTimeout) window.clearTimeout(typeTimeout);
+
+// if (value) {
+//   typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
+// }
