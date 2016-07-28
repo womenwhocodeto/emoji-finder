3a06f55823f4d92de11df4ba95d5eb4ed20e7ea5
Heather Vandervecht <heathervandervecht@gmail.com>
2016-07-27 22:32:40 -0400

    EXERCISE: filter emojis in the ui

--- main.js
+++ main.js
@@ -16,24 +16,14 @@ const removeClass = (el, oldClass) => {
   const inputField = document.getElementsByTagName('input')[0];
   let typeTimeout;
   const TYPE_INTERVAL = 10000;
-  // Set variable for filtering purposes
   const siteContainer = document.getElementsByTagName('body')[0];
 
   inputField.addEventListener("input", (event) => {
     const value = event.target.value;
-    // Set variable of any emojis set to "visible" (this returns elements if a previous search has already been done)
-    const prevFilter = document.getElementsByClassName('visible');
-    if (typeTimeout) window.clearTimeout(typeTimeout);
-  
-    // Reset the previous search results
-    resetOldEmojis(prevFilter);
 
-    if (value) {
-      typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
-    } else {
-      // Because the input has no value (and therefore we're not filtering), let's remove the filtering class from our site container
-      removeClass(siteContainer, 'filtering');
-    }
+    if (typeTimeout) window.clearTimeout(typeTimeout);
+
+    if (value) typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
   }, false);
 
   const matchKeyword = (value) => {
@@ -46,29 +36,11 @@ const removeClass = (el, oldClass) => {
       matchedEmojiTitles.push(obj.keywords[0]);
     });
 
-    // Looping through the array of emojis that match the search term
-    for (const emoji of matchedEmojiTitles) {
-      // Getting the link in index.html that corresponds to each array item
-      const emojiItem = document.querySelector(`[title="${emoji}"]`);
+    // Loop through emoji array
+      // 1. Find <li /> in markup that matches each emoji
+      // 2. Add class: visible
 
-      // If we successfully grabbed the link from index.html
-      if (emojiItem) {
-        // Add a visible class to this emoji
-        addClass(emojiItem.parentElement,'visible');
-      }
-    }
-
-    // Add a filtering class to the site container which will hide all emojis that do NOT have a visible class
-    addClass(siteContainer, 'filtering');
-  }
-
-  // Resetting previous search results
-  const resetOldEmojis = (value) => {
-    // Loop through the array of emojis that was passed to the function
-    for (const prev of value) {
-      // For each one, remove the visible class
-      removeClass(prev, 'visible');
-    };
+    // Add class: filtering to siteContainer to show results!
   }
 
   const filterByKeyword = (value, obj) => {
@@ -81,3 +53,19 @@ const removeClass = (el, oldClass) => {
     window.setTimeout(func, delay);
   }
 })();
+
+
+
+
+
+
+
+
+// FOR REFERENCE: Please don't just copy and paste. How are you going to learn anything then? ;)
+// for (const emoji of matchedEmojiTitles) {
+//   const emojiItem = document.querySelector(`[title="${emoji}"]`);
+
+//   if (emojiItem) {
+//     addClass(emojiItem.parentElement,'visible');
+//   }
+// }
