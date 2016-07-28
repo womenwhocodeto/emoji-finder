f6815d32d2fcc912b0b7e65ad7e6a0f02e1efe43
Heather Vandervecht <heathervandervecht@gmail.com>
2016-07-27 22:12:37 -0400

    fix: removed entire eventListener for first exercise

--- main.js
+++ main.js
@@ -4,13 +4,10 @@
   let typeTimeout;
   const TYPE_INTERVAL = 10000;
 
-  inputField.addEventListener("input", (event) => {
+  // Event Listener: Input
     // 1. Get value of input
-
     // 2. Reset timeout
-
     // 3. Send value onward and set a timeout
-  }, false);
 
   const delay = (func, delay) => {
     window.setTimeout(func, delay);
@@ -21,12 +18,12 @@
 
 
 
+// FOR REFERENCE: Please don't just copy and paste. How are you going to learn anything then? ;)
+// inputField.addEventListener("input", (event) => {
+  // const value = event.target.value;
+  // if (typeTimeout) window.clearTimeout(typeTimeout);
 
-
-
-// const value = event.target.value;
-// if (typeTimeout) window.clearTimeout(typeTimeout);
-
-// if (value) {
-//   typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
-// }
+  // if (value) {
+  //   typeTimeout = delay(matchKeyword(value), TYPE_INTERVAL);
+  // }
+// }, false);
